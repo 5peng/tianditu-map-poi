e418a5d58f54b27cd9d969595ce5cc9387763157
MinoWu <minowu@foxmail.com>
2014-06-11 22:49:10 +0800

    create interests.js

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
+var interests = require('./routes/interests');
 var users = require('./routes/users');
 
 var app = express();
@@ -22,6 +23,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
+app.use('/interests', interests);
 app.use('/users', users);
 
 /// catch 404 and forward to error handler
